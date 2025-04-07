20992b0f8d7f5f50ba971f29d755bd2cc3f68972
Diego Dorrejo <ddorrejo@isf.do>
2025-04-07 13:02:25 -0400

    9. Palindrome Number

---
+++ 9. Palindrome Number.cs
@@ -0,0 +1,16 @@
+//https://leetcode.com/problems/palindrome-number/description/
+
+public class Solution {
+    public bool IsPalindrome(int x) {
+        if(x < 0) return false;
+
+        string str = x.ToString();
+
+        for(int i = 0; i < str.Length / 2; i++){
+            if(str[i] != str[str.Length - 1 - i]){
+                return false;
+            }
+        }
+        return true;
+    }
+}
